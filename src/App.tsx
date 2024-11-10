d320ad1b6f0d1cc85b25b888fc81a0644ab409b5
Dmitriy <616tm616@gmail.com>
2024-11-10 18:40:33 +0300

    Init

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import cls from "./styles/App.module.css";
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <ul className={cls.Header}>
+          <li>
+            <a href="#">home</a>
+          </li>
+          <li>
+            <a href="#">about</a>
+          </li>
+          <li>
+            <a href="#">fdfd</a>
+          </li>
+          <li>
+            <a href="#">fdfd</a>
+          </li>
+          <li>
+            <a href="#">fdfd</a>
+          </li>
+        </ul>
+      </header>
+    </div>
+  );
+};
+
+export default App;
